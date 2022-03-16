09e40e64afd5f0a20a3a6e75587da508cb8f49cc
Cassi Vesuvian <77704245+indychrista@users.noreply.github.com>
2022-03-16 12:58:32 -0400

    Update footer links & styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,40 @@
 import React from "react"
+import logo from "../assets/images/logo.svg"
 
 const Footer = () => {
   return (
     <footer className="page-footer">
+      <img src={logo} alt="logo" />
+      <div>
+        <a href="https://indychrista.com" className="btn footer-btn">
+          Go to indychrista.com
+        </a>
+      </div>
       <div className="footer-social-bar">
-        <span className="icon">F</span>
-        <span className="icon">I</span>
-        <span className="icon">P</span>
+        <a href="https://facebook.com/christa/indychrista">
+          <span className="icon" aria-label="Facebook link">
+            F
+          </span>
+        </a>
+        <a href="https://instagram.com/indychrista">
+          <span className="icon" aria-label="Instagram link">
+            I
+          </span>
+        </a>
+        <a href="https://twitter.com/indychrista">
+          <span className="icon" aria-label="Twitter link">
+            T
+          </span>
+        </a>
       </div>
       <div>
         <p>
           &copy;{new Date().getFullYear()}{" "}
           <span>Christa DeJesus Design & Development</span>.
+        </p>
+        <p>
+          (This website is for demonstration only and does not represent an
+          actual automotive company.)
         </p>
       </div>
     </footer>
